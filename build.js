299766377b26fe08a2a7b26af9b7cb29f66ead36
Diogo Moretti <diogo.pearljam@gmail.com>
2023-02-13 17:49:42 -0300

    initial commit

---
+++ build.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+const colors = require('./src/colors')
+
+const files = ['oceans-color-theme.json']
+
+files.forEach(file => {
+  let text = fs.readFileSync(`./src/${file}`, 'utf8')
+
+  Object.entries(colors).forEach(([colorVar, hexStr]) => {
+    text = text.replace(new RegExp(`\\${colorVar}`, 'g'), hexStr)
+  })
+
+  fs.writeFileSync(`./themes/${file}`, text)
+})
---
+++ src/colors.js
@@ -0,0 +1,19 @@
+module.exports = {
+  '--color-comments': '#444444',
+  '--color-gray': '#EEEEEE',
+  '--color-negative': '#F8F8F8',
+
+  '--color-accent': '#64C68B', // Green
+  '--color-1': '#5AC0E4', // Blue
+  '--color-2': '#A187D9', // Purple
+  '--color-3': '#DC639F', // Pink
+  '--color-4': '#DDD14E', // Yellow
+  '--color-5': '#F3AA3D', // Orange
+
+  '--color-red': '#CD5353',
+
+  '--color-program': '#000000',
+  '--color-widget': '#111111',
+  '--color-opposite': '#FFFFFF',
+  '--color-editor': '#00000060',
+}
